edc1d3830f6eaec90ae0a9de21b4133460e3023f
qwertyuiop161 <idiotmethere@gmail.com>
2026-03-02 23:37:35 -0500

    Switched to Swing for a web based application

---
+++ src/main/java/com/stock/trader/controller/StockController.java
@@ -0,0 +1,21 @@
+package com.stock.trader.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stock.trader.service.StockService;
+
+@RestController
+@RequestMapping("/api")
+public class StockController {
+    private final StockService stockService;
+    public StockController(StockService stockService) {
+        this.stockService = stockService;
+    }
+    @GetMapping("/stock")
+    public String getStock(@RequestParam(defaultValue = "AAPL") String symbol) {
+        return stockService.getDailyStock(symbol);
+    }
+}
---
+++ src/main/java/com/stock/trader/service/StockService.java
@@ -0,0 +1,13 @@
+package com.stock.trader.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class StockService {
+    private final WebClient webClient = WebClient.create("https://www.alphavantage.co");
+    private final String API_KEY = "1JODJUV5UQ859LYX";
+    public String getDailyStock(String symbol) {
+        return webClient.get().uri(uriBuilder -> uriBuilder.path("/query").queryParam("function", "TIME_SERIES_DAILY").queryParam("symbol", symbol).queryParam("apikey", API_KEY).build()).retrieve().bodyToMono(String.class).block();
+    }
+}
