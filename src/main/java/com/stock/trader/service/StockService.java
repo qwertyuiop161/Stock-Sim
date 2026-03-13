d26c9df323d0a76a1c2f4b93ea999af7936189f5
qwertyuiop161 <idiotmethere@gmail.com>
2026-03-13 17:42:01 -0400

    Added candle sticks, but it doesnt work yet. Next commit should include functional candlesticks and fix of parameters like market cap.

--- src/main/java/com/stock/trader/service/StockService.java
+++ src/main/java/com/stock/trader/service/StockService.java
@@ -8,7 +8,10 @@ public class StockService {
     private final WebClient webClient = WebClient.create("https://www.alphavantage.co");
     private final String API_KEY = "1JODJUV5UQ859LYX";
     public String getDailyStock(String symbol) {
-        return webClient.get().uri(uriBuilder -> uriBuilder.path("/query").queryParam("function", "TIME_SERIES_DAILY").queryParam("symbol", symbol).queryParam("apikey", API_KEY).build()).retrieve().bodyToMono(String.class).block();
+        return webClient.get().uri(uriBuilder -> uriBuilder.path("/query").queryParam("function", "TIME_SERIES_DAILY").queryParam("symbol", symbol).queryParam("apikey", API_KEY).build())
+        .retrieve()
+        .bodyToMono(String.class)
+        .block();
     }
     public String searchSymbol(String keywords) {
         return webClient.get()
